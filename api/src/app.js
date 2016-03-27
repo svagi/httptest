d67eccd85eb8e5ce4b032d224c97d0843e993686
svagi <jan@svager.cz>
2016-03-27 14:31:12 +0200

    Separate selenium logic

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,13 @@
+import { generateHAR } from './selenium/firefox'
 import { getProtocols } from './socket'
-import { readFile as readFileNode } from 'fs'
-import { watch } from 'chokidar'
 import bodyParser from 'body-parser'
 import express from 'express'
 import expressValidator from 'express-validator'
-import firefox from 'selenium-webdriver/firefox'
 import morgan from 'morgan'
 import parseUrl from 'url-parse'
-import Promise from 'bluebird'
-import uuid from 'node-uuid'
-import webdriver from 'selenium-webdriver'
 
 export const app = express()
 const isProduction = app.get('env') !== 'development'
-const readFile = Promise.promisify(readFileNode)
 
 // Turn off extra header
 app.disable('x-powered-by')
@@ -44,55 +38,10 @@ app.get('/har', (req, res) => {
   req.checkQuery('protocols').optional().isArray()
   req.sanitizeQuery('url').toURL()
   const errors = req.validationErrors()
-  if (errors) {
-    return res.status(400).send({ errors: errors })
-  }
-  const url = req.query.url.toString()
-  const id = uuid.v4()
-  const directory = process.env.API_HAR_DIR
-  const watcher = watch(directory + id + '.har')
-  const profile = new firefox.Profile()
-  profile.setPreference('app.update.enabled', false)
-  profile.setPreference('devtools.toolbar.enabled', true)
-  profile.setPreference('devtools.cache.disabled', true)
-  profile.setPreference('network.dnsCacheExpiration', 0)
-  profile.setPreference('devtools.netmonitor.enabled', true)
-  profile.setPreference('devtools.netmonitor.har.defaultFileName', id)
-  profile.setPreference('devtools.netmonitor.har.enableAutoExportToFile', true)
-  profile.setPreference('devtools.netmonitor.har.includeResponseBodies', false)
-  profile.setPreference('devtools.netmonitor.har.pageLoadedTimeout', 100)
-  profile.setPreference('devtools.netmonitor.har.defaultLogDir', directory)
-  const options = new firefox.Options()
-    .setProfile(profile)
-  const driver = new webdriver.Builder()
-    .forBrowser('firefox', '45.1')
-    .setFirefoxOptions(options)
-    .build()
-  driver.actions()
-    // Open devtools with ctrl+shift+q (needed for HAR export)
-    .keyDown(webdriver.Key.CONTROL)
-    .keyDown(webdriver.Key.SHIFT)
-    .sendKeys('q')
-    .keyUp(webdriver.Key.SHIFT)
-    .keyUp(webdriver.Key.CONTROL)
-    .perform()
-    // Open url
-    .then(() => driver.get(url))
-    // Wait for exported HAR file
-    .then(() => new Promise((resolve, reject) => {
-      watcher
-        .on('add', resolve)
-        .on('error', reject)
-    }))
-    // Read HAR from disk
-    .then(readFile)
+  if (errors) return res.status(400).send({ errors: errors })
+  return generateHAR(req.query.url.toString())
     // Send HAR as JSON response
     .then((data) => res.send(data))
-    // Cleanup
-    .then(() => {
-      watcher.close()
-      driver.quit()
-    })
     // Error handler
     .catch((err) => {
       console.error(err)
---
+++ api/src/selenium/firefox.js
@@ -0,0 +1,64 @@
+import { readFile as readFileNode } from 'fs'
+import { watch } from 'chokidar'
+import firefox from 'selenium-webdriver/firefox'
+import path from 'path'
+import Promise from 'bluebird'
+import uuid from 'node-uuid'
+import webdriver from 'selenium-webdriver'
+
+const readFile = Promise.promisify(readFileNode)
+
+function createOptions (prefs) {
+  const profile = new firefox.Profile()
+  Object.keys(prefs)
+    .forEach((pref) => profile.setPreference(pref, prefs[pref]))
+  return new firefox.Options().setProfile(profile)
+}
+
+export function generateHAR (url, opts = {}) {
+  const { id = uuid.v4(), dir = process.env.API_HAR_DIR, ext = '.har' } = opts
+  const options = createOptions({
+    'app.update.enabled': false,
+    'devtools.toolbar.enabled': true,
+    'devtools.cache.disabled': true,
+    'network.dnsCacheExpiration': 0,
+    'devtools.netmonitor.enabled': true,
+    'devtools.netmonitor.har.enableAutoExportToFile': true,
+    'devtools.netmonitor.har.includeResponseBodies': false,
+    'devtools.netmonitor.har.pageLoadedTimeout': 100,
+    'devtools.netmonitor.har.defaultFileName': id,
+    'devtools.netmonitor.har.defaultLogDir': dir
+  })
+  const watcher = watch(path.join(dir, id + ext))
+  const driver = new webdriver.Builder()
+    .forBrowser('firefox', '45.1')
+    .setFirefoxOptions(options)
+    .build()
+  return new Promise((resolve, reject) => {
+    driver.actions()
+      // Open devtools with ctrl+shift+q (needed for HAR export)
+      .keyDown(webdriver.Key.CONTROL)
+      .keyDown(webdriver.Key.SHIFT)
+      .sendKeys('q')
+      .keyUp(webdriver.Key.SHIFT)
+      .keyUp(webdriver.Key.CONTROL)
+      .perform()
+      // Open url
+      .then(() => driver.get(url))
+      // Wait for exported HAR file
+      .then(() => new Promise((resolve, reject) => {
+        watcher.on('add', resolve).on('error', reject)
+      }))
+      // Read HAR from disk
+      .then(readFile)
+      // Resolve HAR
+      .then(resolve)
+      // Clean up
+      .then((har) => {
+        console.log('CLEAN')
+        watcher.close()
+        driver.quit()
+      })
+      .catch(reject)
+  })
+}
