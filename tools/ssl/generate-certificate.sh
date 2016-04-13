45e4a1656c6aa78fc57d816dfdbcf7218593e651
svagi <jan@svager.cz>
2016-04-13 16:52:51 +0200

    Add tool for generating self-signed certificates

---
+++ tools/ssl/generate-certificate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate self signed wildcard SSL certificate with alternative names
+openssl req \
+  -new \
+  -newkey rsa:2048 \
+  -sha1 \
+  -days 3650 \
+  -nodes \
+  -x509 \
+  -keyout ssl.key \
+  -out ssl.crt \
+  -config openssl.cnf
