ed11bc3124abc14de2d5dd12929c10ce11e141bb
svagi <jan@svager.cz>
2016-05-08 17:27:06 +0200

    WWW: Remove bundle.js from server push hints

--- www/src/server.js
+++ www/src/server.js
@@ -139,8 +139,7 @@ app.get('*', (req, res) => {
         // TODO move logic somewhere else?
         res.header('Link', [
           '</static/pure.min.css>; rel=preload; as=style;',
-          '</static/bundle.css>; rel=preload; as=style;',
-          '</static/bundle.js>; rel=preload; as=script;'
+          '</static/bundle.css>; rel=preload; as=style;'
         ])
         res.status(200).send(html)
       }
