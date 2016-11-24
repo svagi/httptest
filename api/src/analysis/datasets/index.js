26e3f397e5476ba8bbcfbc8b019a5903c4bb7449
svagi <jan@svager.cz>
2016-11-24 20:05:19 +0100

    Add new dataset "aktualne.cz"

--- api/src/analysis/datasets/index.js
+++ api/src/analysis/datasets/index.js
@@ -6,11 +6,8 @@ function load (filename) {
 }
 
 export const datasets = {
-  'http://seznam.cz': [
-    load('1-seznam.cz.har')
-  ],
-  'http://svager.cz': [
-    load('1-svager.cz.har')
-  ]
+  'http://seznam.cz': [load('1-seznam.cz.har')],
+  'http://svager.cz': [load('1-svager.cz.har')],
+  'http://aktualne.cz': [load('1-aktualne.cz.har')]
 }
 export const domains = Object.keys(datasets)
