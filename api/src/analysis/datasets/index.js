d25db6912bd58cb0c6f64a710d2a289fb39bad6d
svagi <jan@svager.cz>
2016-12-04 17:33:37 +0100

    Remove dataset

--- api/src/analysis/datasets/index.js
+++ api/src/analysis/datasets/index.js
@@ -7,7 +7,6 @@ function load (filename) {
 
 export const datasets = {
   'http://seznam.cz': [load('1-seznam.cz.har')],
-  'http://svager.cz': [load('1-svager.cz.har')],
-  'http://aktualne.cz': [load('1-aktualne.cz.har')]
+  'http://svager.cz': [load('1-svager.cz.har')]
 }
 export const domains = Object.keys(datasets)
